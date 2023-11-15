f9033dafc77c9f248c8958a610599aa0da33d30a
jhaozhou <zhoujiahao0913@outlook.com>
2023-11-15 21:40:49 +0800

    add some comments

--- lab4.py
+++ lab4.py
@@ -1,9 +1,11 @@
+
+# assembly code
 # Loop: fld   f0,0(x1)  //f0=array element
-#   fadd.d  f4,f0,f2  //add scalar in f2
-#   fsd   f4,0(x1)  //store result
-#   addi   x1,x1,8   //decrement pointer
+#       fadd.d  f4,f0,f2  //add scalar in f2
+#       fsd   f4,0(x1)  //store result
+#       addi   x1,x1,8   //decrement pointer
 #          //8 bytes (per DW)
-#   bne   x1,x2,Loop //branch x1 != x2 
+#       bne   x1,x2,Loop //branch x1 != x2 
 
 from prettytable import PrettyTable
 import numpy as np
@@ -17,13 +19,13 @@ class table_entry():
 class Loop_Execution():
     def __init__(self,instructions):
         self.instructions = instructions
-        self.cycle = 0
+
         self.table = []
-        self.wait_cycles = 0
+
         length = len(instructions)
         self.latency_matrix = np.zeros((length+1,length+1),dtype = np.int32)
-        self.distance_matrix = np.ones((length,length),dtype = np.int32)
-    
+        
+    # 判断指令类型
     def is_FP_ALU_Op(self,Ir):
         if(Ir[0][0]=='f'):
             op = Ir[0][1:]
@@ -52,6 +54,7 @@ class Loop_Execution():
                 return True
         return False
 
+    # 打印结果
     def table_print(self):
         tb = PrettyTable()
         tb.field_names=["clk","ir"]
@@ -60,31 +63,32 @@ class Loop_Execution():
         print(tb)
     
     def run(self):
+        #第一步更新邻接矩阵、latency_matrix[i][j]即为第i个与第j个指令之间的延迟数
         for i in range(len(self.instructions)):
             ir1 = self.instructions[i]
             if ir1[0]=="bne" and i==(len(self.instructions)-1) :
                 self.latency_matrix[i][i+1]=1
             for j in range(i+1,len(self.instructions)):
                 ir2 = self.instructions[j]
-                print("ir1:",ir1)
-                print("ir2:",ir2)
+                #print("ir1:",ir1)
+                #print("ir2:",ir2)
                 if self.is_FP_ALU_Op(ir1):
                     if self.is_FP_ALU_Op(ir2):
                         self.latency_matrix[i][j] = 3
-                        print('3')
+                        #print('3')
                     elif self.is_Store_double(ir2):
                         self.latency_matrix[i][j] =2
-                        print('2')
+                        #print('2')
                 elif self.is_Load_double(ir1):
                     if self.is_FP_ALU_Op(ir2):
                         self.latency_matrix[i][j]=1
-                        print('1')
+                        #print('1')
                 elif self.is_Integer_Op(ir1):
                     if ir2[0]=="bne":
                         if ir1[1]==ir2[1] or ir1[1] == ir2[2]:
                             self.latency_matrix[i][j]=1
-                            print('1')
-                
+                            #print('1')
+        #第二步通过第i、j个指令之间的延迟数与第i、j指令之间的物理距离，来更新第i、i+1之间的延迟数
         for i in range(len(self.instructions)):
             distance = 0
             for j in range(i+1,len(self.instructions)):
@@ -92,7 +96,8 @@ class Loop_Execution():
                 if distance <= self.latency_matrix[i][j]:
                     self.latency_matrix[i][i+1] += (self.latency_matrix[i][j]-distance+1)
                     distance += (self.latency_matrix[i][j]-distance+1)
-        print("latency_matrix:",self.latency_matrix)
+        
+        #通过第i、j个指令之间的延迟打印整个执行过程
         clock = 0
         for i in range(len(self.instructions)):
             ir = self.instructions[i]
@@ -118,5 +123,9 @@ instructions_scheduled = [["fld","f0","0","x1"],["addi","x1","x1","8"],
 
 # print(instructions_unscheduled)
 # print(instructions_scheduled)
-loop_execution = Loop_Execution(instructions_unscheduled)
-loop_execution.run()
+print("unscheduled:")
+loop_execution_unscheduled = Loop_Execution(instructions_unscheduled)
+loop_execution_unscheduled.run()
+print("scheduled:")
+loop_execution_scheduled = Loop_Execution(instructions_scheduled)
+loop_execution_scheduled.run()
